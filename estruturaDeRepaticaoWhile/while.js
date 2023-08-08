d7178ca33a066ec1561c0d5d70b0db33d22def6d
Geovane Bausen <movinghallucination@gmail.com>
2023-08-08 09:49:41 -0300

    estruturaDeRepetiçãoWhileEdoWhile

---
+++ estruturaDeRepaticaoWhile/while.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+while (c <= 6) {
+  console.log("tudo bem?");
+  c++;
+}*/
+//estrutura de repetição com teste lógico no inicio
+
+do{
+  var c = 1
+  console.log("tudo bem?");
+  c++;
+}while(c <= 6)
+
+/*lembre-se que não precisa por = depois do console.log*/
