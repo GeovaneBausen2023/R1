44e029590acf939ded7bcb2c1de57ce9e8bf6078
Geovane Bausen <movinghallucination@gmail.com>
2023-08-11 08:59:45 -0300

    arraysConsoleMini

---
+++ estruturaDeRepaticaoComVariavelDeControle(for)/for.js
@@ -0,0 +1,8 @@
+//estrutura de repetição com variável de controle
+console.log("start...");
+for (var c = 1; c <= 5; c++){
+  console.log(c);
+}
+console.log("end...");
+/*lembre-se que não precisa por = ; depois do ++*/
+
