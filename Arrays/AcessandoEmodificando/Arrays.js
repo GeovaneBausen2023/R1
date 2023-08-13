301418f068da0aa9fa5ada60d019e3cc67bc7110
Geovane Bausen <movinghallucination@gmail.com>
2023-08-13 13:51:10 -0300

    arraysEstudo

--- Arrays/AcessandoEmodificando/Arrays.js
+++ Arrays/AcessandoEmodificando/Arrays.js
@@ -1,6 +1,7 @@
 //ex:como criar um array para adcionar elemento de um estoque"
 /*Um array em JavaScript pode conter uma variedade de tipos de dados,
   incluindo variáveis, números, strings, objetos e até mesmo outros arrays.*/
+    //criando um array
 console.log("Elementos do array:")
 var estoqueMolhos = ["mShoyu", "mTeriaque", "mAgridoce", "mBarbecue"];
 console.log(estoqueMolhos)
@@ -8,13 +9,17 @@ console.log(estoqueMolhos)
 console.log("Ex. de como inserir outros tipos de dados:")
 var random = ["string", 795, [0, 1, 2]];
 console.log(random)
-//
+
+//acessando elementos atraves do index
 console.log("Acessando elementos do array:")
 console.log("Elemento na posição 0:")
 console.log(random[0]);
 console.log("Elemento na posição 2:")
-console.log(random[2]);
-//
+console.log(random[1]);
+console.log("ultimo elemento:")
+console.log (random.length -1);
+
+//modificando elementos
 console.log("modificando um item do array:")
 estoqueMolhos[0] = "mShoyuHiperConcetrado";
 console.log('Elemento na posição 0: "estoqueMolhos"')//observe como inserir aspas dentro do console.log
---
+++ Arrays/copiandoUmArray/SpliceCopy.js
@@ -0,0 +1,4 @@
+var frutas = ["Maçã", "Banana"];
+
+var copiar = frutas.slice(); // é assim que se copia
+console.log(copiar)
