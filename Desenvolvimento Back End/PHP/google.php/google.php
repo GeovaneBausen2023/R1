6ced27cdcb13677837023deb1edf490f60c5de2e
Geovane Bausen <movinghallucination@gmail.com>
2023-08-15 09:56:25 -0300

    recriandoHomePageDoGooglePHP

---
+++ Desenvolvimento Back End/PHP/google.php/google.php
@@ -0,0 +1,53 @@
+<?php 
+echo '
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>google by Geo</title>
+    <link rel="stylesheet" href="style google/css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif&family=Roboto:wght@300;400;700&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="header">
+        <a href="#" class="link">Gmail</a>
+        <a href="#" class="link">Imagens</a>
+        <ion-icon name="apps-outline"></ion-icon>
+        <a href="#" class="link btn">Fazer Login</a>
+
+    </div>
+    <div class="main">
+      <img src="style google/img/logoGoogle.png" alt="google">
+        <input class="pesquisa" type="text">
+        <div class="botoes">
+            <a href="#" class="btn-cinza">Pesquisa Google</a>
+            <a href="#" class="btn-cinza">Estou com sorte</a>
+        </div>
+    </div>
+
+    <div class="footer">Brasil</div>
+    <div class="footer1">
+        <a href="#" class="link">Sobre</a>
+        <a href="#" class="link">Publicidade</a>
+        <a href="#" class="link">Negócios</a>
+        <a href="#" class="link">Como funciona a pesquisa</a>
+        <a href="#" class="link">Privacidade</a>
+        <a href="#" class="link">Termos</a>
+        <a href="#" class="link">Configurações</a>
+    </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>';
+?>
+
